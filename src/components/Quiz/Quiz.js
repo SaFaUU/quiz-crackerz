c59d726d75bb3b2b2368e655d073481426678584
SaFaUU <thulk283@gmail.com>
2022-10-12 00:30:03 +0600

    Right and Wrong Answer Check

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import QuizQuestion from '../QuizQuestion/QuizQuestion';
 import './Quiz.css'
@@ -7,15 +7,19 @@ const Quiz = () => {
     const quiz_data = useLoaderData();
     const { data } = quiz_data;
     const { name, questions } = data;
+    const [totalCorrect, setTotalCorrect] = useState(0);
     return (
         <div className='container-fluid'>
             <div className='quiz-bg py-2 w-75 mx-auto mt-5 rounded text-white'>
                 <h2 className='mb-0'>Quiz Page for: <span className='fw-bold'>{name}</span></h2>
+                <p className='mt-2 mb-0'>Correct Answer: {totalCorrect}</p>
             </div>
             {
                 questions.map(question => <QuizQuestion
                     key={question.id}
                     question={question}
+                    totalCorrect={totalCorrect}
+                    setTotalCorrect={setTotalCorrect}
                 ></QuizQuestion>)
             }
         </div>
--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
-import QuizAnswers from '../QuizAnswers/QuizAnswers';
+import React, { useEffect, useState } from 'react';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import { ButtonToolbar, ToggleButtonGroup } from 'react-bootstrap';
 
 
-const QuizQuestion = ({ question }) => {
+const QuizQuestion = ({ question, totalCorrect, setTotalCorrect }) => {
     const [radioValue, setRadioValue] = useState('1');
-    console.log(question);
+
+
+    let correctAnswer;
+
+    useEffect(() => {
+        if (radioValue === question.correctAnswer) {
+            setTotalCorrect(totalCorrect + 1);
+        }
+    }, [radioValue]);
+
+    if (radioValue === question.correctAnswer) {
+        correctAnswer = 'success';
+
+    }
+    else {
+        correctAnswer = 'danger';
+    }
     // console.log(question.options[0]);
     return (
-        <div className='quiz-bg py-2 w-75 mx-auto mt-5 rounded text-white'>
+        <div className='py-4 w-75 mx-auto mt-5 rounded text-black border shadow-sm'>
             <h3 className='mb-0 ps-3 py-2 px-2 fs-5 text-start' dangerouslySetInnerHTML={{ __html: question.question }} />
-            {/* {
-                question.options.map((option, idx) =>
-                    <QuizAnswers
-                        key={idx}
-                        option={option}
-                        id={`${idx}-${question.id}`}
-                    >
-
-                    </QuizAnswers>)
-            } */}
             <ToggleButtonGroup name={question.id}>
                 {
                     question.options.map((option, idx) => (
@@ -34,7 +39,7 @@ const QuizQuestion = ({ question }) => {
                                     name="radio"
                                     value={option}
                                     checked={radioValue === option}
-                                    variant={radioValue === option ? 'primary' : 'light'}
+                                    variant={radioValue === option ? correctAnswer : 'light'}
                                     onChange={(e) => setRadioValue(e.currentTarget.value)}
                                 // className='text-black bg-light me-2'
                                 >
